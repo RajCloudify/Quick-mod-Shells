05a2a6cbcaaf169522007032488d8908468fabe1
Raj Attingal <rajattingal1@gmail.com>
2025-03-15 20:01:12 +0400

    Sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -9,6 +9,6 @@ chown sonar:sonar sonarqube-8.9.6.50800 -R
 chmod 777 sonarqube-8.9.6.50800 -R
 su - sonar
 
-#run this on server manually
+#Run this on server manually.
 #sh /opt/sonarqube-8.9.6.50800/bin/linux/sonar.sh start
 #echo "user=admin & password=admin"
