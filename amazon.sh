1da53589c6d2139d0f634dbcb32b8b565dbef4b4
Raj Attingal <rajattingal1@gmail.com>
2026-04-09 23:59:18 +0400

    Fix mv command syntax in amazon.sh

--- amazon.sh
+++ amazon.sh
@@ -4,5 +4,5 @@ systemctl start httpd
 systemctl status httpd
 cd /var/www/html
 git clone https://github.com/Ironhack-Archive/online-clone-amazon.git
-mv online-clone-amazon/* .
+mv online-clone-amazon/* . 
 tail -f /var/log/httpd/access_log
