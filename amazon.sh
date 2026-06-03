cff9ea74fe30b995a0133194c0d563a69b7fa341
Raj Attingal <rajattingal1@gmail.com>
2026-06-03 21:44:29 +0530

    Fix formatting and update commands in amazon.sh

--- amazon.sh
+++ amazon.sh
@@ -2,7 +2,7 @@
 yum install httpd git -y
 systemctl start httpd
 systemctl status httpd
-cd /var/www/html
+cd /var/www/html 
 git clone https://github.com/Ironhack-Archive/online-clone-amazon.git
 mv online-clone-amazon/* . 
 tail -f /var/log/httpd/access_log
